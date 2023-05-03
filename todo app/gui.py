e7222445cedd817fc388278516c8b1402538a4f4
Kanhaiya <kanhaiyayadav93@gmail.com>
2023-05-04 00:51:30 +0530

    Complete version

--- todo app/gui.py
+++ todo app/gui.py
@@ -1,15 +1,15 @@
-import functions1
+import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
-                      enable_events=True, size=[45,10])
+                      enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button],[list_box, edit_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -34,7 +34,7 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
         case "todos":
-           window['todo'].update(value=values['todos'][0])
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
